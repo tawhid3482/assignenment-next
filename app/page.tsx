c2a08192c73c6d6a9905250b261edc59d993dfcd
Tawhidul Islam <tawhidulislam3482@gmail.com>
2026-05-23 17:13:30 +0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <div className="p-10">
-      <h1 className="text-2xl font-bold">Frontend Running 🚀</h1>
+      <h1 className="text-2xl font-bold">Frontend Running dfsa das🚀</h1>
 
       <ul className="mt-5">
         {data.map((item: any, index) => (
